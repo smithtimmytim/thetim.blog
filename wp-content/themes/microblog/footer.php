3e5a9ff140be5151788f15181d7bff30f19e0aa5
Timothy B. Smith <smith@ttimsmith.com>
2017-02-17 02:19:56 -0600

    Add some small stuff

--- wp-content/themes/microblog/footer.php
+++ wp-content/themes/microblog/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-  <small class="copyright">&copy; <?php echo date('Y'); ?> Timothy B. Smith</small>
+  <small class="copyright">&copy; <?php echo date('Y'); ?> <a href="<?php echo site_url(); ?>">Timothy B. Smith</a>.</small>
 </footer>
 
 <?php wp_footer(); ?>
--- wp-content/themes/microblog/header.php
+++ wp-content/themes/microblog/header.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
+  <title>Tim Smith’s Microblog</title>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+  <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" />
 
   <!-- Cloud Typography -->
   <link rel="stylesheet" type="text/css" href="//cloud.typography.com/6973314/6451952/css/fonts.css" />
@@ -12,7 +15,7 @@
   <!-- Global -->
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/global.css"/>
 
-  <title>Tim Smith’s Microblog</title>
+  <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico">
   <?php wp_head(); ?>
 </head>
 
